0777315ad45f66a75d648d3900bac4e529b8ab5e
aschlichter <adipostaja@gmail.com>
2021-12-12 10:02:55 +0100

    HTTP Response created. Closes #4.

---
+++ src/main/java/com/example/server/model/Response.java
@@ -0,0 +1,25 @@
+package com.example.server.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.experimental.SuperBuilder;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+/**
+ * Created by adam on 2021. 12. 12.
+ */
+@Data
+@SuperBuilder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Response {
+    protected LocalDateTime timeStamp;
+    protected int statusCode;
+    protected HttpStatus status;
+    protected String reason;
+    protected String message;
+    protected String developerMessage;
+    protected Map<?, ?> data;
+}
