b2759621ea3d7176e46012faf74e8101be1f82eb
aschlichter <adipostaja@gmail.com>
2021-12-12 09:52:04 +0100

    Random Server images created. Closes #5.

--- src/main/java/com/example/server/service/implementation/ServerServiceImpl.java
+++ src/main/java/com/example/server/service/implementation/ServerServiceImpl.java
@@ -8,11 +8,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.transaction.Transactional;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.util.Collection;
+import java.util.Random;
 
 import static com.example.server.enumeration.Status.SERVER_DOWN;
 import static com.example.server.enumeration.Status.SERVER_UP;
@@ -73,6 +75,8 @@ public class ServerServiceImpl implements ServerService {
     }
 
     private String setServerImageUrl() {
-        return null;
+        String[] imageNames = {"server1.png", "server2.png", "server3.png", "server4.png"};
+        return ServletUriComponentsBuilder.fromCurrentContextPath().path("/server/image/" +
+                imageNames[new Random().nextInt(4)]).toUriString();
     }
 }
