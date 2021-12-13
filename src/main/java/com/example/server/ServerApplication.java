bc85516424b04df5567a95dccd7ed0adcb4090e4
aschlichter <adipostaja@gmail.com>
2021-12-13 20:14:20 +0100

    HTTP Resources created. Closes #6.

--- src/main/java/com/example/server/ServerApplication.java
+++ src/main/java/com/example/server/ServerApplication.java
@@ -1,13 +1,30 @@
 package com.example.server;
 
+import com.example.server.enumeration.Status;
+import com.example.server.model.Server;
+import com.example.server.repo.ServerRepo;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import static com.example.server.enumeration.Status.*;
 
 @SpringBootApplication
 public class ServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner run(ServerRepo serverRepo) {
+        return args -> {
+            serverRepo.save(new Server(null, "192.168.1.111", "Ubuntu Linux", "16 GB",
+                    "Personal Computer", "http://localhost:8080/server/image/server1.png", SERVER_UP));
+            serverRepo.save(new Server(null, "192.168.1.111", "Raspbian", "32 GB",
+                    "Web Server", "http://localhost:8080/server/image/server2.png", SERVER_UP));
+        };
+    }
 
 }
